684d4dfe8458ed23966dd212f4a0693e5d326954
DylanOleary <31968480+DylanOleary@users.noreply.github.com>
2018-09-12 19:33:15 -0400

    Changes:
    1. Set buttons to "Yes" by default
    2. Set default length of password to 5 characters
    
    Additions:
    
    1. Feature to exclude duplicate characters
    2. Feature to allow user to copy password to clipboard
    3. Feature to allow user to clear the password field
    
    Style:
    
    1. Added a basic background theme

--- src/logic/PasswordGenerator.java
+++ src/logic/PasswordGenerator.java
@@ -1,5 +1,6 @@
 package logic;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class PasswordGenerator {
@@ -7,12 +8,14 @@ public class PasswordGenerator {
     private String password;
     private int passwordLength;
     private Random random;
+    private boolean duplicateDecision;
 
-    public PasswordGenerator(int passwordLength, boolean specialCharacters){
+    public PasswordGenerator(int passwordLength, boolean specialCharacters, boolean capitalLetters, boolean duplicateDecision){
         this.password = "";
         this.characters = "";
         setPasswordLength(passwordLength);
-        setCharacters(specialCharacters);
+        setCharacters(specialCharacters, capitalLetters);
+        this.duplicateDecision = duplicateDecision;
         this.random = new Random();
     }
 
@@ -36,21 +39,49 @@ public class PasswordGenerator {
 
     //This method determines whether or not the password will contain special characters
 
-    public void setCharacters(boolean specialCharacters){
+    public void setCharacters(boolean specialCharacters, boolean capitalLetters){
+        // We first decide whether we want special characters in the password
+
         if(specialCharacters == true){
             characters = "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*";
         }else{
             characters = "abcdefghijklmnopqrstuvwxyz123456789";
+        }
+
+        //Next, we decide if we want capital letters in the password
+
+        if(capitalLetters == true){
+            characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + characters;
         }
     }
 
     //This method builds the password string
 
     public void buildPassword(){
-        for(int i = 0; i < passwordLength; i++) {
-            int characterChooser = random.nextInt(characters.length()); //Chooses a random number from 0 - # of chars
 
-            password += characters.charAt(characterChooser);
+        //This if/else block determines whether or not we build the password with duplicate characters
+
+        if(!duplicateDecision == true){
+            ArrayList<Integer> chosenNumbers = new ArrayList<>();
+
+            for(int i = 0; i < passwordLength; i++){
+                int characterChooser = random.nextInt(characters.length()); //Chooses a random number from 0 - # of chars
+
+                while(chosenNumbers.contains(characterChooser)){
+                    characterChooser = random.nextInt(characters.length());
+                }
+
+                if(!chosenNumbers.contains(characterChooser)){
+                    password+= characters.charAt(characterChooser);
+                    chosenNumbers.add(characterChooser);
+                }
+            }
+        }else{
+            for(int i = 0; i < passwordLength; i++) {
+                int characterChooser = random.nextInt(characters.length()); //Chooses a random number from 0 - # of chars
+
+                password += characters.charAt(characterChooser);
+            }
         }
     }
 
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,6 +9,10 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import logic.PasswordGenerator;
 
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,15 +21,35 @@ public class Controller implements Initializable {
 
     @FXML
     private ChoiceBox lengthChoiceBox;
+
+    //Special characters button
     @FXML
-    private RadioButton yes;
+    private RadioButton specialYes;
     @FXML
-    private RadioButton no;
+    private RadioButton specialNo;
+
+    //Capital letters button
+    @FXML
+    private RadioButton capitalYes;
+    @FXML
+    private RadioButton capitalNo;
+
+    //Duplicate letters button
+    @FXML
+    private RadioButton duplicateYes;
+    @FXML
+    private RadioButton duplicateNo;
+
     @FXML
     private Button generatePassword;
     @FXML
     private TextField showPassword;
+    @FXML
+    private Button copyPassword;
+    @FXML
+    private Button clearPassword;
 
+    /** Generates a password for the user */
     @FXML
     public void generatePasswordButtonPushed(){
 
@@ -42,22 +66,60 @@ public class Controller implements Initializable {
         boolean specialCharactersDecision = true;
 
 
-        if(yes.isSelected()){
+        if(specialYes.isSelected()){
             specialCharactersDecision = true;
         }
 
-        if(no.isSelected()){
+        if(specialNo.isSelected()){
             specialCharactersDecision = false;
         }
 
+        /** This block assigns either true or false to the PasswordGenerator with respect to including capital
+         * letters in the password
+         */
 
+        boolean capitalLettersDecision = true;
+
+        if(capitalYes.isSelected()){
+            capitalLettersDecision = true;
+        }
+
+        if(capitalNo.isSelected()){
+            capitalLettersDecision = false;
+        }
+
+        /** This block determines whether the password will be built to exclude duplicate characters */
+        boolean duplicateDecision = true;
+
+        if(duplicateYes.isSelected()){
+            duplicateDecision = false;
+        }
+
+        if(duplicateNo.isSelected()){
+            duplicateDecision = true;
+        }
 
         /** The password is built */
-        PasswordGenerator password = new PasswordGenerator(passwordLengthArgument, specialCharactersDecision);
+        PasswordGenerator password = new PasswordGenerator(passwordLengthArgument, specialCharactersDecision, capitalLettersDecision, duplicateDecision);
         password.buildPassword();
 
-
         showPassword.setText(password.getPassword());
+    }
+
+    /** Copies the password to the system clipboard */
+    @FXML
+    public void copyPasswordButtonPushed(){
+        String passwordToBeCopied = showPassword.getText();
+        StringSelection stringSelection = new StringSelection(passwordToBeCopied);
+
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+    }
+
+    /** Clears the TextField where the generated password is shown */
+    @FXML
+    public void clearPasswordButtonPushed(){
+        showPassword.setText("");
     }
 
     /**
@@ -67,6 +129,7 @@ public class Controller implements Initializable {
     public void initialize(URL url, ResourceBundle rb)
     {
         showPassword.setText("");
+        lengthChoiceBox.setValue(5);
 
         for(int i = 5; i <=20; i++){
             lengthChoiceBox.getItems().add(i);
