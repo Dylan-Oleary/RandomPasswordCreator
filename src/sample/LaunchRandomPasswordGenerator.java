439c53a9aaee25068663e2d3eb458631c92640f2
DylanOleary <31968480+DylanOleary@users.noreply.github.com>
2018-09-17 02:37:45 -0400

    Added more CSS styling

--- src/sample/LaunchRandomPasswordGenerator.java
+++ src/sample/LaunchRandomPasswordGenerator.java
@@ -1,12 +1,16 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class LaunchRandomPasswordGenerator extends Application {
+    @FXML
+    private Circle themeSelector;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
